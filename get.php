b75af741d84b931851998b068c8fb2b30cbf7913
Tyler Stokes <tylerstokes8040@gmail.com>
2017-04-26 22:29:29 -0500

    get init

---
+++ get.php
@@ -0,0 +1,14 @@
+<?php
+$mysqli = new mysqli("localhost", "webservice", "webservice", "task");
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+echo $mysqli->host_info . "\n";
+
+$mysqli = new mysqli("127.0.0.1", "webservice", "webservice", "task", 3306);
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+echo $mysqli->host_info . "\n";
+?>
