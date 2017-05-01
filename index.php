abf0ba4a6604b71c1ce80f201f7261795a677175
Tyler Stokes <tylerstokes8040@gmail.com>
2017-04-30 20:48:39 -0500

    Application page

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+  <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+  <script type="text/javascript" src="js/app.js"></script>
+</head>
+<body>
+</body>
+</html>
