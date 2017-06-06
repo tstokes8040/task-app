95f1ea95497a3abc32baf2a25608b1ad0e941d1e
Tyler Stokes <tylerstokes8040@gmail.com>
2017-06-06 13:03:23 -0500

    Updated 6/6/17

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
-$(function() {
-  $.getJSON("http://localhost/task-app/app.php",
-    function(data) {
-      console.log(data);
+//Get Tasks
+function getTasks(){
+  let theRequest = new XMLHttpRequest();
+  theRequest.open("GET", "http://localhost/task-app/get-tasks.php", true);
+  theRequest.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      let theData = JSON.parse(XMLHttpRequest.responseText)
+      console.log(theData);
+      let data = theData;
+      let $tasks = document.querySelector('.task-list');
+      for (let i = 0; i < data.length; i++) {
+        $tasks.innerHTML += `<div class="task">${data[i].name}</div>`;
+      }
     }
-  );
-});
+  }
+  theRequest.send();
+}
+
+getTasks();
